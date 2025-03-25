69eea0453d0923f3249a40efc7d425772b17a71c
Barrett Ruth <br.barrettruth@gmail.com>
2025-03-24 21:06:34 -0400

    feat(etc): actually mee the problem statement requirements

--- src/etc.cc
+++ src/etc.cc
@@ -2,17 +2,44 @@
 
 #include <cstddef>
 #include <iostream>
+#include <limits>
+#include <utility>
 
 #include "arm.hh"
 #include "etc.hh"
 #include "statistics.hh"
+
+constexpr size_t EXPERIMENT_REPITIONS = 5;
+
+std::pair<size_t, long double> best_m(size_t T, size_t K) {
+  size_t best;
+  long double best_avg_reward = std::numeric_limits<long double>::min();
+  for (size_t m = 1; m <= T / K; ++m) {
+    long double total_reward = 0;
+
+    for (size_t i = 0; i < EXPERIMENT_REPITIONS; ++i) {
+      ETCAgent agent(K, T);
+      size_t best_arm = agent.explore(m);
+      long double reward = agent.commit(best_arm);
+
+      total_reward += reward;
+    }
+
+    long double avg_reward = total_reward / EXPERIMENT_REPETITION;
+    if (avg_reward > best_avg_reward) {
+      best = m;
+      best_avg_reward = avg_reward;
+    }
+  }
+
+  return {m, best_reward};
+}
 
 int main() {
   constexpr size_t MIN_K = 10, MAX_K = 30;
   constexpr size_t K_STEP = 5;
   constexpr size_t MIN_T = 1000, MAX_T = 5000;
   constexpr size_t T_STEP = 1000;
-  constexpr size_t EXPERIMENT_REPITIONS = 5;
 
   for (size_t K = MIN_K; K <= MAX_K; K += K_STEP) {
     for (size_t T = MIN_T; T <= MAX_T; T += T_STEP) {
@@ -23,7 +50,7 @@ int main() {
 
         std::vector<long double> rewards;
         for (size_t i = 0; i < EXPERIMENT_REPITIONS; ++i) {
-          ETCAgent agent(K);
+          ETCAgent agent(K, T);
           size_t best_arm = agent.explore(m);
           long double overall_reward = agent.commit(best_arm);
           rewards.emplace_back(overall_reward);
@@ -31,10 +58,16 @@ int main() {
 
         auto stdev = Statistics::get_stdev(rewards);
         auto mean = Statistics::get_mean(rewards);
-        std::cout << "Got overall reward stdev: " << stdev << "; mean: " << mean << '\n';
+        std::cout << "Got overall reward stdev: " << stdev << "; mean: " << mean
+                  << '\n';
       }
     }
   }
+
+  size_t T = 5000, K = 30;
+  auto [m, best_reward] = best_m(T, K);
+  std::cout << "With T=" << T << "; K=" << K << ", got best m=" << m << '\n';
+  std::cout << "It has a highest average reward of " << best_reward << '\n';
 
   return 0;
 }
